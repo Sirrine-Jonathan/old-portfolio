e485e0eb6f5084933c3e237f48b56a4ea1723b7c
Jonathan <Enirrisky@gmail.com>
2017-02-07 20:25:21 -0800

    changed style

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 window.onload = function(){
 	for(let l=0; l<studyTopics.length;l++){
+		//hard coded element
 		let list = document.getElementById('list');
-		let li = document.createElement('li');
-		let anchor = document.createElement('a');
-		let text = document.createTextNode(studyTopics[l]);
+		
+		//dynamic content
+		let span = document.createElement('span');
+		let rendering = studyTopics[l].toString().split(',').join(' ');
+		let text = document.createTextNode(rendering);
 		let arr = studyTopics[l][0].split(' ');
 		let href = 'pages/'+arr[0]+'HOME.html';
 		let linkDiv = document.createElement('div');
 		linkDiv.setAttribute('class','linkDiv');
-		anchor.setAttribute('href',href);
-		anchor.appendChild(text);
+		//anchor.setAttribute('href',href);
+		span.appendChild(text);
+		span.setAttribute('class','linkText');
 		
 		//new things
-		anchor.addEventListener('mouseenter', function(){
+		linkDiv.addEventListener('mouseenter', function(){
 			document.getElementById('previewFrame').src = href;
+			this.style.borderColor = 'black';
 		});	
-		anchor.addEventListener('mouseleave', function(){
+		linkDiv.addEventListener('mouseleave', function(){
 			document.getElementById('previewFrame').src = 'previewIntro.html';
-			
+			this.style.borderColor = '#33ff66';
 		});
 		
-		linkDiv.appendChild(anchor);
-		li.appendChild(linkDiv);
-		list.appendChild(li);
+		linkDiv.addEventListener('click', function(){
+			location.href = href;
+		});
+		
+		linkDiv.appendChild(span);
+		
+		//add dynamic content to hardcoded element
+		list.appendChild(linkDiv);
 	}
 }
